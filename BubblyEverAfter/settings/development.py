59c1a69f22976e6fae669f2388e2920d2b576aa5
Georges Haddad <georges7haddad@gmail.com>
2020-04-03 19:47:41 +0300

    Repo Setup

---
+++ BubblyEverAfter/settings/development.py
@@ -0,0 +1,4 @@
+from BubblyEverAfter.settings.common import *
+
+DEBUG = True
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
---
+++ BubblyEverAfter/settings/production.py
@@ -0,0 +1,5 @@
+from BubblyEverAfter.settings.common import *
+
+
+DEBUG = False
+ALLOWED_HOSTS = ["0.0.0.0"]
