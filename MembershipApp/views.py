8a2cf4f8ccaf58d137f2980524b5032fb7369638
Georges Haddad <georges7haddad@gmail.com>
2020-04-05 20:13:26 +0300

    [add] membership login_required decorator

--- MembershipApp/views.py
+++ MembershipApp/views.py
@@ -1,12 +1,11 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 
 from .models import BubblyMember
-
-...
 
 
 def login_request(request):
@@ -34,9 +33,11 @@ def login_request(request):
     return render(request=request, template_name="Membership/members_login.html", context={"form": form})
 
 
+@login_required(login_url="/member/login")
 def member_profile(request):
     return render(request, "Membership/member_profile.html")
 
 
+@login_required(login_url="/member/login")
 def leader_profile(request):
     return render(request, "Membership/leader_profile.html")
